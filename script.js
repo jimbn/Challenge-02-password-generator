bfd75dd38b6bd16b268458adc2ef570b46fd8d28
Jim Nguyen <95522881+jimbn@users.noreply.github.com>
2022-01-16 16:56:23 -0800

    added confirmation alerts

--- script.js
+++ script.js
@@ -3,18 +3,82 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
+var generatePassword = function() {
+  
+  var passMin = validMin ();
+    if (passMin === false) {
+      alert("Please only input valid NUMERALS!");
+      validMin();
+    }
+  
+  var passMax = validMax ();
+    if (passMax === false) {
+      alert("Please only input valid NUMERALS!");
+      validMax();
+    }  
+
+  // Questionaire to see check of character specification
+  var cLowercase = window.confirm("Do you want your password to include LOWERCASE letters?");
+    // Alert to notify user if the password will or will not include the characters
+    if (cLowercase) {
+      alert("Your password will include LOWERCASE letters!");
+    } else {
+      alert("Your password will not include LOWERCASE letters!");
+    }
+  var cUppercase = window.confirm("Do you want your password to include UPPERCASE letters?");
+    if (cUppercase) {
+      alert("Your password will include UPPERCASE letters!");
+    } else {
+      alert("Your password will not include UPPERCASE letters!");
+    }
+  var cNumbers = window.confirm("Do you want your password to include NUMBERS?");
+    if (cNumbers) {
+      alert("Your password will include NUMBERS!");
+    } else {
+      alert("Your password will not include NUMBERS!");
+    }
+  var cSpecialCharacters = window.confirm("Do you want your password to include SPECIAL CHARACTERS?");
+    if (cSpecialCharacters) {
+      alert("Your password will include SPECIAL CHARACTERS!");
+    } else {
+      alert("Your password will not include SPECIAL CHARACTERS!");
+    }
+
+   // Pull  min and max from above to generate random passcode
+  var passwordLength = randomLength (passMin, passMax);
+
+  // Set strings of possible characters based on questionaire.
+  let pCharacters = ""
+    if (cLowercase) pCharacters = pCharacters.concat(characters.lowercase.join(""));
+    if (cUppercase) pCharacters = pCharacters.concat(characters.uppercase.join(""));
+    if (cNumbers) pCharacters = pCharacters.concat(characters.numbers.join(""));
+    if (cSpecialCharacters) pCharacters = pCharacters.concat(characters.specialCharacters.join(""));
+
+  // Generate password based on random selection of characters choosen from selected character array
+  let gPassword = "";
+  for (let i = 0; i < passwordLength; i++) {
+    gPassword += pCharacters.charAt (
+      Math.floor(Math.random()*pCharacters.length)
+    );
+  }
+
+  return gPassword;
+}
+
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
 // Generate a random length for specified minimum and maximum length
 var randomLength = function(min, max) {
   var value = Math.floor(Math.random() * (max - min + 1) + min);
   return value;
-};
-
-// Arrays of different type of characters can be used
-var characters = {
-  lowercase: ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
-  uppercase: ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-  numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-  specialCharacters: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", "'", ":", '"', "|", ",", ".", "<", ">", '/', "?"],
 };
 
 // Function to validate if prompt input is a number. Returns false if NaN.
@@ -26,6 +90,7 @@ var validation = function(num) {
   }
 }
 
+// Function to return answer if it is a number. Returns false 
 var validMin = function() {
   var minPrompt = prompt("What is the MINIMUM length of the password (PLEASE ONLY USE NUMERALS)?");
     var isValid = validation (minPrompt);
@@ -43,60 +108,13 @@ var validMax = function () {
     return parseInt(maxPrompt);
     } else {
     return false;
-  }
+   }
 }
 
-var generatePassword = function() {
-  
-  var passMin = validMin ();
-    if (passMin === false) {
-      alert("Please only input valid NUMERALS!")
-      validMin();
-    } 
-  
-  var passMax = validMax ();
-    if (passMax === false) {
-      alert("Please only input valid NUMERALS!")
-      validMax();
-    }   
-
-
-  // Questionaire to see check of character specification
-  var cLowercase = window.confirm("Have password contain LOWERCASE?"); 
-  var cUppercase = window.confirm("Have password contain UPPERCASE?");
-  var cNumbers = window.confirm("Have password contain NUMBERS?");
-  var cSpecialCharacters = window.confirm("Have password contain SPECIAL CHARACTERS?");
-
-  // Pull from above to generate random passcode
-  var passwordLength = randomLength (passMin, passMax);
-
-  // Set strings of possible characters based on questionaire.
-  let pCharacters = ""
-    if (cLowercase) pCharacters = pCharacters.concat(characters.lowercase.join(""));
-    if (cUppercase) pCharacters = pCharacters.concat(characters.uppercase.join(""));
-    if (cNumbers) pCharacters = pCharacters.concat(characters.numbers.join(""));
-    if (cSpecialCharacters) pCharacters = pCharacters.concat(characters.specialCharacters.join(""));
-
-  let gPassword = "";
-  for (let i = 0; i < passwordLength; i++) {
-    gPassword += pCharacters.charAt (
-      Math.floor(Math.random()*pCharacters.length)
-    );
-  }
-
-  return gPassword;
-
+// Arrays of different type of characters can be used
+var characters = {
+  lowercase: ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  uppercase: ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+  numbers: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  specialCharacters: ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "[", "]", "{", "}", ";", "'", ":", '"', "|", ",", ".", "<", ">", '/', "?"],
 };
-
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-};
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
